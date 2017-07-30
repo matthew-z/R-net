328f5f29554c2be2615391f1a1fb4f057a791865
zhaohao zeng <zhaohaozeng@zhzeng.local>
2017-07-30 14:52:50 +0900

    gave up torch.text pre-processing

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,111 +1,111 @@
-# -*- coidng: utf-8 -*-
-
-"""
-dataset(dict):
-    data(list):
-       paragraphs(list):
-            context(str):
-            qas(list):
-                answers(list):  training set has 1 answer, where dev set has 3 answers
-                    answer_start(int):
-                    text(str):
-                id(str):
-                question(str):
-       title(str):
-    version(str):
-"""
-
-import numpy as np
-
+import functools
 import json
-import tensorflow as tf
-import io
-import spacy
-
+import os
+import random
 from collections import Counter
 
-version = "1.1"
-min_count = 5
-VOCAB_SIZE = 50000
-ENCODER_INPUT_LEN = 440
-DECODER_INPUT_LEN = 413
+import dill
+import nltk
+import progressbar
+import spacy
+import torch
+from torchtext import data
 
-spacy_en = spacy.load('en')
-
-tokenizer = lambda s: [tok.text for tok in spacy_en.tokenizer(s)]
-
-
-_PAD = "_PAD"
-_UNK = "_UNK"
-_SPL = "_SPL"
-_START_VOCAB = [_PAD, _UNK, _SPL]
-
-PAD_ID = 0
-UNK_ID = 1
-SPL_ID = 2
-
-_B = "_B"
-_I = "_I"
-_O = "_O"
-_GO = "_GO"
-_EOS = "_EOS"
-_DECODE_VOCAB = [_PAD, _B, _I, _O, _GO, _EOS]
-
-B_ID = 1
-I_ID = 2
-O_ID = 3
-GO_ID = 4
-EOS_ID = 5
+# only draw 100 examples for developing/debugging
+DEBUG = True
 
 
-def load_bin_vec(fname):
-    embedding = []
-    word2idx = {}
-    idx2word = {}
-    with open(fname, "rb") as f:
-        header = f.readline()
-        vocab_size, emb_size = list(map(int, header.split()))
-        binary_len = np.dtype('float32').itemsize * emb_size
-        for line in range(vocab_size):
-            word = []
-            ch = f.read(1)
-            while ch != ' ':
-                if ch != '\n':
-                    word.append(ch)
-                ch = f.read(1)
-
-            word = ''.join(word)
-            embedding.append(np.fromstring(f.read(binary_len), dtype='float32'))
-            word2idx[word] = line
-            idx2word[line] = word
-
-        embedding = np.array(embedding)
-
-    return embedding, word2idx, idx2word
+def read_train_json(path):
+    with open(path) as fin:
+        data = json.load(fin)
+    lines = []
+    for topic in data["data"]:
+        title = topic["title"]
+        for p in topic['paragraphs']:
+            qas = p['qas']
+            context = p['context']
+            for qa in qas:
+                question = qa["question"]
+                answers = qa["answers"]
+                for ans in answers:
+                    answer_start = int(ans["answer_start"])
+                    answer_text = ans["text"]
+                    lines.append([title, context, question, answer_start, answer_text])
+    return lines[:100] if DEBUG else lines
 
 
-def parse_dataset(train_path, dev_path):
-    with open(train_path, "r") as f_train, open(dev_path, "r") as f_dev:
-        train_set = json.load(f_train)
-        dev_set = json.load(f_dev)
-        assert (train_set['version'] == version and dev_set['version'] == version)
-        return [train_set, dev_set]
+def read_dev_json(path):
+    with open(path) as fin:
+        data = json.load(fin)
+    lines = []
+    for topic in data["data"]:
+        title = topic["title"]
+        for p in topic['paragraphs']:
+            qas = p['qas']
+            context = p['context']
+            for qa in qas:
+                question = qa["question"]
+                answers = qa["answers"]
+                question_id = qa["id"]
+                answer_start_list = [ans["answer_start"] for ans in answers]
+                c = Counter(answer_start_list)
+                most_common_answer, freq = c.most_common()[0]
+                answer_text = None
+                if freq > 1:
+                    for i, ans_start in enumerate(answer_start_list):
+                        if ans_start == most_common_answer:
+                            answer_text = answers[i]["text"]
+                            break
+                else:
+                    answer_text = answers[random.choice(range(len(answers)))]["text"]
+
+                lines.append([title, context, question, question_id, answer_text])
+    return lines[:100] if DEBUG else lines
 
 
-def parse2text(datasets, text_path):
-    with io.open(text_path, "w", encoding="utf-8") as f_text:
-        text_list = []
-        for dataset in datasets:
-            for article in dataset['data']:
-                for paragraph in article['paragraphs']:
-                    text_list.append(paragraph['context'])
-                    for question in paragraph['qas']:
-                        text_list.append(question['question'])
-        f_text.write("\n".join(text_list))
+def read_test_json(path):
+    with open(path) as fin:
+        data = json.load(fin)
+    lines = []
+    for topic in data["data"]:
+        title = topic["title"]
+        for p in topic['paragraphs']:
+            qas = p['qas']
+            context = p['context']
+            for qa in qas:
+                question = qa["question"]
+                answers = qa["answers"]
+                question_id = qa["id"]
+                answer_start_list = [ans["answer_start"] for ans in answers]
+                c = Counter(answer_start_list)
+                most_common_answer, freq = c.most_common()[0]
+                answer_text = None
+
+                if freq > 1:
+                    for i, ans_start in enumerate(answer_start_list):
+                        if ans_start == most_common_answer:
+                            answer_text = answers[i]["text"]
+                            break
+                else:
+                    answer_text = answers[random.choice(range(len(answers)))]["text"]
+                lines.append([title, context, question, question_id, answer_text])
+
+    return lines[:100] if DEBUG else lines
 
 
+def tokenized_by_answer(context, text, start, tokenizer):
+    """
+    Locate the answer token-level position after tokenizing as the original location is based on
+    char-level
 
-def tokenized_by_answer(context, text, start):
+    snippet from: https://github.com/haichao592/squad-tf/blob/master/preprocess.py
+
+    :param context:  passage
+    :param text:     context/passage
+    :param start:    answer start position (char level)
+    :param tokenizer: tokenize function
+    :return: tokenized passage, answer start index, answer end index (inclusive)
+    """
     fore = context[:start]
     mid = context[start: start + len(text)]
     after = context[start + len(text):]
@@ -123,115 +123,122 @@ def tokenized_by_answer(context, text, start):
     words.extend(tokenized_fore)
     words.extend(tokenized_mid)
     words.extend(tokenized_after)
-    indies = list(range(len(tokenized_fore),len(tokenized_fore) + len(tokenized_mid)))
-    return words, indies
-
-def padding(sent, target_len):
-    sent = ["</s>"] + sent + ["</b>"] * (target_len - len(sent)) + ["</e>"]
-    return sent
-
-def build_corpus(datasets):
-    seq_len = 766 # + 2
-    q_len = 60
-    a_len = 46
-    vocab = Counter()
-    train_set = []
-    dev_set = []
-    c = Counter()
-
-    for tokenized_set, dataset in zip([train_set, dev_set], datasets):
-        for article in dataset['data']:
-            for paragraph in article['paragraphs']:
-                context = paragraph['context']
-                for qa in paragraph['qas']:
-                    id = qa['id']
-                    question = qa['question']
-                    answer = qa['answers'][0]
-                    text = answer['text']
-                    start = int(answer['answer_start'])
-
-                    return_val = tokenized_by_answer(context, text, start)
-                    if return_val:
-                        tokenized_context, indies = return_val
-                        # if len(indies) > 40 or len(tokenized_context) > 413:
-                        #     continue
-                        tokenized_question = tokenizer(question)
-                        c.update([len(tokenized_context) + len(tokenized_question)])
-                    #     tokenized_context = padding(tokenized_context, seq_len)
-                    #     tokenized_question = padding(tokenized_question, q_len)
-                        vocab.update(tokenized_context)
-                        vocab.update(tokenized_question)
-                        tokenized_set.append((tokenized_context, tokenized_question, indies))
-                    else:
-                        print("tokenize error")
-    print(c)
-    return train_set, dev_set, vocab
+    answer_start_token, answer_end_token = len(tokenized_fore), len(tokenized_fore) + len(tokenized_mid) - 1
+    return words, answer_start_token, answer_end_token
 
 
-def build_vocab(vocab):
-    new_vocab = {}
-    reversed_vocab = {}
-    for index, word in enumerate(_START_VOCAB):
-        new_vocab[word] = index
-        reversed_vocab[index] = word
-    for index, (word, count) in enumerate(vocab.most_common(VOCAB_SIZE - len(_START_VOCAB)), len(_START_VOCAB)):
-        new_vocab[word] = index
-        reversed_vocab[index] = word
-    return new_vocab, reversed_vocab
+def extract_examples_fields(json_path, mode, tokenizer):
+    if tokenizer == "spacy":
+        spacy_en = spacy.load("en")
+        tokenizer = lambda s: [tok.text for tok in spacy_en.tokenizer(s)]
+    elif tokenizer is None:
+        tokenizer = nltk.word_tokenize
+    if mode == "train":
+        extract_method = functools.partial(extract_train_examples, tokenizer=tokenizer)
 
-def build_vector(seq, vocab):
-    vec = [vocab[w] if w in vocab else vocab[_UNK] for w in seq]
-    return vec
+        fields = [("passage", data.Field(tokenize=tuple)),
+                  ("question", data.Field(tokenize=tokenizer)),
+                  ("answer_start", data.Field(sequential=False, use_vocab=False, preprocessing=int)),
+                  ("answer_end", data.Field(sequential=False, use_vocab=False, preprocessing=int)),
+                  # ("answer_text", data.Field(tokenize=tokenize))
+                  ]
 
-def build_qa_pairs(dataset, vocab):
-    encoder_inputs = []
-    decoder_inputs = []
-    for context, question, indies in dataset:
-        encoder_input = question + [_SPL] + context
-        # encoder_pad = [_PAD]*(ENCODER_INPUT_LEN - len(encoder_input))
-        encoder_inputs.append(build_vector(encoder_input, vocab))
+    elif mode == "dev":
+        extract_method = extract_test_examples
+        fields = [("passage", data.Field(tokenize=tokenizer)),
+                  ("question", data.Field(tokenize=tokenizer)),
+                  ("question_id", data.Field(sequential=False, use_vocab=False)),
+                  # ("answer_text", data.Field(tokenize=tokenize))
+                  ]
 
-        decoder_input = []
-        for i in range(len(context)):
-            if i in indies:
-                decoder_input.append(I_ID) #I
-            else:
-                decoder_input.append(O_ID) #O
-        decoder_input[indies[0]] = B_ID # B
-        decoder_inputs.append(decoder_input)
-    return encoder_inputs, decoder_inputs
+    elif mode == 'test':
+        extract_method = extract_test_examples
+        fields = [("passage", data.Field(tokenize=tokenizer)),
+                  ("question", data.Field(tokenize=tokenizer)),
+                  ("question_id", data.Field(sequential=False, use_vocab=False))]
+
+    else:
+        raise ValueError("Incorrect mode %s for building dataset" % mode)
+    examples = extract_method(fields, json_path)
+    return examples, fields
 
 
+def extract_test_examples(fields, path):
+    examples = []
+    bar = progressbar.ProgressBar()
+    lines = read_test_json(path)
+    for _, context, question, question_id in bar(lines):
+        ex = data.Example.fromlist([context, question, question_id],
+                                   fields)
+        examples.append(ex)
+    return examples
 
-def main():
-    # embedding, word2idx, idx2word = load_bin_vec("./data/vectors")
 
-    datasets = parse_dataset('./data/squad/train-v1.1.json', './data/squad/dev-v1.1.json')
-    # parse2text(datasets, "./data/text.txt")
+def extract_train_examples(fields, path, tokenizer):
+    """
 
-    train_set, dev_set, vocab = build_corpus(datasets)
-    vocab, reversed_vocab = build_vocab(vocab)
+    :param fields: define how to pre-process data. See torchtext.data.Field
+    :param path:   json file location
+    :param tokenizer: tokenizer function
+    :return: torchtext.data.Example object list
+    """
+    bar = progressbar.ProgressBar()
+    examples = []
+    lines = read_train_json(path)
+    for _, context, question, answer_start, answer_text in bar(lines):
+        passage_tokens, answer_start_token, answer_end_token = tokenized_by_answer(context, answer_text,
+                                                                                   answer_start, tokenizer)
+        ex = data.Example.fromlist([passage_tokens, question, answer_start_token, answer_end_token], fields)
+        examples.append(ex)
+    return examples
 
-    with io.open("./data/vocab%d.en" % VOCAB_SIZE, "w", encoding="utf-8") as f_vocab:
-        vocab_list = sorted(vocab, key=vocab.get)
-        for word in vocab_list:
-            f_vocab.write(word + str("\n"))
+def get_dataset(json_path, cache_root="./data/cache", mode="train", tokenizer=None):
+    fields_file = os.path.join(cache_root, "%s%s.fields" % (mode, "_DEBUG" if DEBUG else ""))
+    examples_file =  os.path.join(cache_root,"%s%s.examples" % (mode, "_DEBUG" if DEBUG else ""))
 
-    train_enc_inputs, train_dec_inputs = build_qa_pairs(train_set, vocab)
-    dev_enc_inputs , dev_dec_inputs = build_qa_pairs(dev_set, vocab)
-    with tf.gfile.GFile("./data/train.source", mode="w") as train_source:
-        with tf.gfile.GFile("./data/train.target", mode="w") as train_target:
-            for input in train_enc_inputs:
-                train_source.write(" ".join(map(str,input)) + "\n")
-            for input in train_dec_inputs:
-                train_target.write(" ".join(map(str,input)) + "\n")
+    # read examples and fields if they exist
+    if os.path.exists(fields_file) and os.path.exists(examples_file):
+        fields = dill.load(open(fields_file, "rb"))
+        examples = dill.load(open(examples_file,"rb"))
+        squad_dataset = data.Dataset(examples, fields)
+        squad_dataset.sort_key = lambda x: (len(x.question), len(x.passage))
 
-    with tf.gfile.GFile("./data/dev.source", mode="w") as dev_source:
-        with tf.gfile.GFile("./data/dev.target", mode="w") as dev_target:
-            for input in dev_enc_inputs:
-                dev_source.write(" ".join(map(str,input)) + "\n")
-            for input in dev_dec_inputs:
-                dev_target.write(" ".join(map(str,input)) + "\n")
+    else:
+        # build examples otherwise
+        examples, fields_for_building = extract_examples_fields(json_path, mode, tokenizer)
+        squad_dataset = data.Dataset(examples, fields_for_building)
 
-if __name__ == "__main__":
-    main()
+        # build voc
+        passage_field = squad_dataset.fields["passage"]
+        question_field = squad_dataset.fields["question"]
+        passage_field.build_vocab(squad_dataset, [x.question for x in squad_dataset.examples],
+                                                    wv_type='glove.840B', wv_dir="./data/embedding/glove_word/",
+                                                    unk_init="zero")
+        question_field.vocab = squad_dataset.fields["passage"].vocab
+
+        fields = squad_dataset.fields
+        dill.dump(examples, open(examples_file, "wb"))
+        dill.dump(fields, open(fields_file, "wb"))
+
+    # set sort_key for bucketing
+    squad_dataset.sort_key = lambda x: (len(x.passage))
+    return squad_dataset
+
+
+def get_iter_for_training(batch_size=128, device=None, tokenizer=None, cache_root="./data/cache"):
+    train_json = "./data/squad/train-v1.1.json"
+    dev_json = "./data/squad/dev-v1.1.json"
+
+    train_dataset = get_dataset(json_path=train_json, mode="train", tokenizer=tokenizer, cache_root=cache_root)
+    dev_dataset = get_dataset(json_path=dev_json, mode="dev", tokenizer=tokenizer, cache_root=cache_root)
+
+    train_iter, dev_iter = data.BucketIterator.splits(
+        (train_dataset, dev_dataset), batch_size=batch_size, device=device)
+
+    return train_iter, dev_iter
+
+
+if __name__ == "__name__":
+    train_json = "./data/squad/train-v1.1.json"
+    train = get_dataset(json_path=train_json, mode="train")
+    train_iter = data.BucketIterator(train, batch_size=12, device=-1)
