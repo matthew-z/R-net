597b232bbea019e208e8fdcf5c447dacd03e30dd
zhaohao <zhaohao.zeng93@gmail.com>
2018-12-16 16:13:12 +0900

    add dataset

---
+++ qa/squad/dataset.py
@@ -0,0 +1,122 @@
+import logging
+from typing import Dict, Tuple, Dict, List
+
+from allennlp.common.file_utils import cached_path
+from allennlp.data import DatasetReader, Tokenizer, TokenIndexer, Token, Instance
+from allennlp.data.dataset_readers.reading_comprehension.util import make_reading_comprehension_instance, \
+    char_span_to_token_span
+from allennlp.data.token_indexers import SingleIdTokenIndexer
+from allennlp.data.tokenizers import WordTokenizer
+from overrides import overrides
+import json
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
+
+
+
+@DatasetReader.register("squad_truncated")
+class SquadReader(DatasetReader):
+    """
+    Modified from allennlp.data.dataset_readers.reading_comprehension.squad
+
+    Reads a JSON-formatted SQuAD file and returns a ``Dataset`` where the ``Instances`` have four
+    fields: ``question``, a ``TextField``, ``passage``, another ``TextField``, and ``span_start``
+    and ``span_end``, both ``IndexFields`` into the ``passage`` ``TextField``.  We also add a
+    ``MetadataField`` that stores the instance's ID, the original passage text, gold answer strings,
+    and token offsets into the original passage, accessible as ``metadata['id']``,
+    ``metadata['original_passage']``, ``metadata['answer_texts']`` and
+    ``metadata['token_offsets']``.  This is so that we can more easily use the official SQuAD
+    evaluation script to get metrics.
+
+    Parameters
+    ----------
+    tokenizer : ``Tokenizer``, optional (default=``WordTokenizer()``)
+        We use this ``Tokenizer`` for both the question and the passage.  See :class:`Tokenizer`.
+        Default is ```WordTokenizer()``.
+    token_indexers : ``Dict[str, TokenIndexer]``, optional
+        We similarly use this for both the question and the passage.  See :class:`TokenIndexer`.
+        Default is ``{"tokens": SingleIdTokenIndexer()}``.
+    """
+    def __init__(self,
+                 tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 lazy: bool = False,
+                 max_passage_len=400,
+                 truncate_train_only=True) -> None:
+        super().__init__(lazy)
+        self._tokenizer = tokenizer or WordTokenizer()
+        self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
+        self.max_passage_len = max_passage_len
+        self.truncate_train_only = truncate_train_only
+
+    @overrides
+    def _read(self, file_path: str):
+        if "train" in file_path:
+            is_train = True
+        else:
+            is_train = False
+
+        # if `file_path` is a URL, redirect to the cache
+        file_path = cached_path(file_path)
+
+        logger.info("Reading file at %s", file_path)
+        with open(file_path) as dataset_file:
+            dataset_json = json.load(dataset_file)
+            dataset = dataset_json['data']
+        logger.info("Reading the dataset")
+        for article in dataset:
+            for paragraph_json in article['paragraphs']:
+                paragraph = paragraph_json["context"]
+                tokenized_paragraph = self._tokenizer.tokenize(paragraph)
+
+                if len(tokenized_paragraph) > self.max_passage_len:
+                    if is_train or not self.truncate_train_only:
+                        continue
+
+                for question_answer in paragraph_json['qas']:
+                    question_text = question_answer["question"].strip().replace("\n", "")
+                    answer_texts = [answer['text'] for answer in question_answer['answers']]
+                    span_starts = [answer['answer_start'] for answer in question_answer['answers']]
+                    span_ends = [start + len(answer) for start, answer in zip(span_starts, answer_texts)]
+                    instance = self.text_to_instance(question_text,
+                                                     paragraph,
+                                                     zip(span_starts, span_ends),
+                                                     answer_texts,
+                                                     tokenized_paragraph)
+                    yield instance
+
+    @overrides
+    def text_to_instance(self,  # type: ignore
+                         question_text: str,
+                         passage_text: str,
+                         char_spans: List[Tuple[int, int]] = None,
+                         answer_texts: List[str] = None,
+                         passage_tokens: List[Token] = None) -> Instance:
+        # pylint: disable=arguments-differ
+        if not passage_tokens:
+            passage_tokens = self._tokenizer.tokenize(passage_text)
+        char_spans = char_spans or []
+
+        # We need to convert character indices in `passage_text` to token indices in
+        # `passage_tokens`, as the latter is what we'll actually use for supervision.
+        token_spans: List[Tuple[int, int]] = []
+        passage_offsets = [(token.idx, token.idx + len(token.text)) for token in passage_tokens]
+        for char_span_start, char_span_end in char_spans:
+            (span_start, span_end), error = char_span_to_token_span(passage_offsets,
+                                                                         (char_span_start, char_span_end))
+            if error:
+                logger.debug("Passage: %s", passage_text)
+                logger.debug("Passage tokens: %s", passage_tokens)
+                logger.debug("Question text: %s", question_text)
+                logger.debug("Answer span: (%d, %d)", char_span_start, char_span_end)
+                logger.debug("Token span: (%d, %d)", span_start, span_end)
+                logger.debug("Tokens in answer: %s", passage_tokens[span_start:span_end + 1])
+                logger.debug("Answer: %s", passage_text[char_span_start:char_span_end])
+            token_spans.append((span_start, span_end))
+
+        return make_reading_comprehension_instance(self._tokenizer.tokenize(question_text),
+                                                        passage_tokens,
+                                                        self._token_indexers,
+                                                        passage_text,
+                                                        token_spans,
+                                                        answer_texts)
